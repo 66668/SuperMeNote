aad9ad8add8b1e0843920240047933b8d57cfb0a
66668 <sjy0118atsn@163.com>
2018-08-20 16:05:27 +0800

    Initial commit

---
+++ app/src/main/java/com/superme/sjynote/MainActivity.java
@@ -0,0 +1,16 @@
+package com.superme.sjynote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 主界面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
